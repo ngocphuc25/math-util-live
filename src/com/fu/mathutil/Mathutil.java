2f845c3652dc17fa6561052bef3ced244a94d2d4
ngocphuc25 <tranminhngocphuc16@gmail.com>
2023-09-12 23:47:57 +0700

    first commit

---
+++ src/com/fu/mathutil/Mathutil.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.fu.mathutil;
+
+/**
+ *
+ * @author tranm
+ */
+public class Mathutil {
+    public String Call(){
+      String a;
+      a= "goi dc cai chuoi r";
+        return a;
+    }
+}
